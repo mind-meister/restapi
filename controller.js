a178cfdead406aa36badc702600514bd1b6daffc
charlesprabowo <charles_prabowo@yahoo.co.id>
2021-12-12 14:15:25 +0700

    update

--- controller.js
+++ controller.js
@@ -2,5 +2,5 @@ const response = require('./res');
 const connection = require('./koneksi')
 
 exports.index = function(req,res){
-    response.ok('Aplikasi REST API berjalan!')
+    response.ok('Aplikasi REST API berjalan!',res)
 }
