898c459ceb5d4c87ae51aeca11404c6352695e0f
charlesprabowo <charles_prabowo@yahoo.co.id>
2021-12-12 13:59:14 +0700

    menambahkan file koneksi

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
+
 // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
