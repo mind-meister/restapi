c310cf8dbe73f121fcfa39f6dc6050fa48415708
charlesprabowo <charles_prabowo@yahoo.co.id>
2021-12-12 14:23:32 +0700

    menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -1,6 +1,17 @@
-const response = require('./res');
-const connection = require('./koneksi')
+const response = require("./res");
+const connection = require("./koneksi");
 
-exports.index = function(req,res){
-    response.ok('Aplikasi REST API berjalan!',res)
-}
+exports.index = function (req, res) {
+  response.ok("Aplikasi REST API berjalan!", res);
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilDataMahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
--- router.js
+++ router.js
@@ -2,4 +2,5 @@ module.exports = function (app) {
   const jsonku = require("./controller");
 
   app.route("/").get(jsonku.index);
+  app.route("/tampil").get(jsonku.tampilDataMahasiswa);
 };
