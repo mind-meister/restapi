13fdafabadcec098c082b5f770377364d5f0587a
charlesprabowo <charles_prabowo@yahoo.co.id>
2021-12-12 14:02:27 +0700

    menambahkan kode res

---
+++ res.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const res = require("express/lib/response");
+
+exports.ok = function(values, res){
+    const data = {
+        'status': 200,
+        'values':values
+    };
+    res.json(data);
+    res.end();
+}
